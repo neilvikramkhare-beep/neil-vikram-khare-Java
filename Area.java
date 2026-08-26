b9ab101c0488951127b9edb461ef2e7fbc4020e0
neilvikramkhare-beep <neilvikramkhare@gmail.com>
2026-08-26 05:16:31 +0000

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Area
+{
+    void square()
+    {
+        int s = 37;
+        System.out.print("Area of Square:"+s*s);
+    }
+    void rectangle()
+    {
+        int l = 25;
+        int b = 15;
+        System.out.print("Area of Rectangle:"+l*b);
+    }
+    void triangle()
+    {
+        int h = 20;
+        int q = 10;
+        System.out.print("Area of Triangle:"+0.5*h*q);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,13 +1,8 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Account account = new Account();
-        account.getdata();
-        
-        System.out.println("\nAccount Details");
-        System.out.println("Balance: " + account.balance);
-        System.out.println("Deposit: " + account.deposit);
-        System.out.println("Check Balance: " + account.checkBalance);
+public class Main {
+    public static void main(String[] args) {
+        Area a = new Area();
+        a.square();
+        a.rectangle();
+        a.triangle();
     }
 }
